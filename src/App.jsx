42996666427107162af73b3f41b9bbbe158a0d46
hyunbinjlee <hyunbinlee@u.northwestern.edu>
2024-10-12 13:13:45 -0500

    Delete testcomponent file and from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
 
-import TestComponent from './components/test'
-
 function App() {
 
   return (
     <div class="flex flex-row min-h-screen justify-center items-center">
       {/* <h1 class="text-3xl font-medium">OMNIPOTENT CTECH BOT</h1> */}
-      <TestComponent />
     </div>
   )
 }
