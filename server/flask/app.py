53ff9f1a1c5a2f87e338eae8b8d3f247a0af5f62
Jovy <zjjovyy8215@gmail.com>
2024-10-12 16:30:29 -0500

    changed apppy

--- server/flask/app.py
+++ server/flask/app.py
@@ -3,17 +3,16 @@ import firebase_admin
 from firebase_admin import credentials, db
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', r'C:\Users\mhenr\Documents\Github\ctec-bot\server\openai')))
+
+path_to_append = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'server', 'openai'))
+sys.path.append(path_to_append)
 
 from API_Prompting import process_evaluations
 
 # Initialize Firebase
-cred = credentials.Certificate(r"C:\Users\mhenr\Downloads\ctec-snap-firebase-adminsdk-x90z8-ac3e68052c.json")
+cred = credentials.Certificate(r"firebase-adminsdk.json")
 firebase_admin.initialize_app(cred, {'databaseURL':'https://ctec-snap-default-rtdb.firebaseio.com'})
 ref = db.reference('/')
-
-
-
 
 app = Flask(__name__)
 
@@ -21,13 +20,11 @@ app = Flask(__name__)
 def hello():
   return "Hello World!"
 
-
 @app.route("/gptResponse", methods=['GET'])
 def gptResponse():
    return process_evaluations(ref.get())
 
 #   return process_evaluations(json_file)
-
 
 if __name__ == "__main__":
   app.run()
